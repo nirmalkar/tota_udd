777825b0e81e720c9690ab8908d2b8e5878182ca
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-28 17:02:16 +0530

    static game obj

--- src/constant/gameObj.js
+++ src/constant/gameObj.js
@@ -2,26 +2,26 @@ export const game = [
   {
     name: "parrot",
     can_fly: true,
-    image: new URL(`../../assets/gameImage/parrot.png`, import.meta.url).href,
+    image_url: new URL(`../../assets/gameImage/parrot.png`, import.meta.url).href,
   },
   {
     name: "bus",
     can_fly: true,
-    image: new URL(`../../assets/gameImage/bus.png`, import.meta.url).href,
+    image_url: new URL(`../../assets/gameImage/bus.png`, import.meta.url).href,
   },
   {
     name: "car",
     can_fly: true,
-    image: new URL(`../../assets/gameImage/car.png`, import.meta.url).href,
+    image_url: new URL(`../../assets/gameImage/car.png`, import.meta.url).href,
   },
   {
     name: "aircraft",
     can_fly: true,
-    image: new URL(`../../assets/gameImage/aircraft.png`, import.meta.url).href,
+    image_url: new URL(`../../assets/gameImage/aircraft.png`, import.meta.url).href,
   },
   {
     name: "ghost",
     can_fly: true,
-    image: new URL(`../../assets/gameImage/ghost.png`, import.meta.url).href,
+    image_url: new URL(`../../assets/gameImage/ghost.png`, import.meta.url).href,
   },
 ];
