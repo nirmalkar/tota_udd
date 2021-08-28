939ab840235bbdb4d937cf5cd8b1e03dd02ecd7b
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-28 17:04:01 +0530

    show img and animation logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "../scss/main.scss";
-import { game } from "./constant/gameObj"
+import { game } from "./constant/gameObj";
 
 const backgroundImagesArr = [
     "bg1",
@@ -21,32 +21,82 @@ const backgroundImagesArr = [
     "bg17",
 ];
 
-let start = true
-let randomIdxInterval 
+let start = false;
+let randomIdxInterval;
+let randomGameIdxInterval;
+let intGameIdx = parseInt(Math.random() * game.length);
+
+const gameScreen = document.querySelector(".game_screen");
+const startButton = document.getElementById("start_game");
 
 function selectRandomIdx(start) {
-    if(start){
+    if (start) {
         randomIdxInterval = setInterval(() => {
             let randomInt = parseInt(Math.random() * backgroundImagesArr.length);
-            
-            document.body.style.backgroundImage = `url(${new URL(`../assets/bg/${backgroundImagesArr[randomInt]}.svg`, import.meta.url)
-                .href
-            })`;
-            console.log(game[gameCurrentIdx]);
+            document.body.style.backgroundImage = `url(${new URL(
+                `../assets/bg/${backgroundImagesArr[randomInt]}.svg`,
+                import.meta.url
+            ).href
+                })`;
         }, 700);
-    }else{
+    } else {
         clearInterval(randomIdxInterval);
     }
 }
-selectRandomIdx(start)
 
-
-document.getElementById("start_game").addEventListener("click", () => {
-    if(start){
-        start = false
-        selectRandomIdx(start)
-    }else{
-        start = true
-        selectRandomIdx(start)
+function startGame(start) {
+    console.log(start);
+    let count = 0
+    addGameImg(count)
+    if (start) {
+        randomGameIdxInterval = setInterval(() => {
+            count++
+            // let gameCurrentIdx = parseInt(Math.random() * game.length);
+            // document.querySelector("#game_img_div").appendChild(gameImg[gameCurrentIdx]?.image_url)
+            removeGameImage()
+            console.log(count);
+        }, 2000);
+    } else {
+        clearInterval(randomGameIdxInterval);
     }
+}
+
+selectRandomIdx(true);
+
+function addGameImg(count){
+    if(count === 0){
+        const gameImg = document.createElement("img")
+        gameImg.id = "game_img"
+        gameImg.setAttribute("src",`${game[intGameIdx].image_url}`)
+        let gameImgDiv = document.querySelector("#game_img_div")
+        gameImgDiv.appendChild(gameImg)
+        game_img_div.style.height = "100%"
+        game_img_div.style.width = "100%"
+        game_img_div.style.transform = "translateX(90%)"
+
+    }else{
+        document.querySelector("#game_img").src = game[intGameIdx].image_url
+    }
+}
+
+function removeGameImage(){
+    let currentGameImg = document.getElementById("game_img")
+    currentGameImg.src = ""
+}
+
+
+startButton.addEventListener("click", () => {
+    startButton.classList.add("hidden");
+    const uddButton = document.createElement("button");
+    uddButton.innerHTML = "UDD!";
+    uddButton.classList.add("start_game_button");
+    uddButton.style.width = "auto";
+    uddButton.style.padding = "20px";
+    uddButton.style.borderRadius = "50px";
+    uddButton.setAttribute("id", "udd_button");
+    function insertAfter(referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+    insertAfter(gameScreen, uddButton);
+    startGame(!start);
 });
