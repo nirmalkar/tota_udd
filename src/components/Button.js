5154228c30f526fea69b783a90c82532e7d2fff6
nirmalkar <hemantnirmalkar17@gmail.com>
2021-09-03 22:13:35 +0530

    made code more readable

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,20 +11,25 @@ export const Button = (buttonProps) => {
         id,
         styles,
     } = buttonProps
-    console.log(referenceNode)
+
     const button = document.createElement('button')
+
     button.classList.add(type)
     classes?.forEach((className, i) => {
         button.classList.add(className)
     })
+
     button.addEventListener('click', (e) => {
         e.preventDefault()
         onClickFunc(button)
     })
+
     button.setAttribute('id', id)
     button.style.display = hidden ? 'none' : 'block'
     button.innerHTML = innerText
+
     Object.assign(button.style, styles)
+
     // insert after function
     function insertAfter(referenceNode, newNode) {
         referenceNode.parentNode.insertBefore(
